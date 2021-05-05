7dfbb06a9069c2eb43e6d7086f605c5841483e6b
Cordsenboyz <46309019+Cordsenboyz@users.noreply.github.com>
2021-05-05 13:57:04 +0200

    ObjecterGrundProg

---
+++ ObjecterGrundlæggendeProg/ObjecterGrundlæggendeProg/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ObjecterGrundlæggendeProg
+{
+    public class BankAccount
+    {
+        private static decimal balance = 400;
+
+        public static void Withdraw()
+        {
+            Console.WriteLine("Input Wanted Amount for Withdrawl");
+            string userinput = Console.ReadLine();
+            decimal withdrawl = Convert.ToDecimal(userinput);
+            if (withdrawl < 0)
+            {
+                withdrawl = withdrawl * -1;
+            }
+            if (withdrawl <= balance)
+            {
+                Console.WriteLine("You Withdrew" + " " + withdrawl);
+            }
+            else
+            {
+                Console.WriteLine("You cannot do that");
+            }
+        }
+
+        public static void Deposit()
+        {
+            Console.WriteLine("Input Wanted Amount for Deposit");
+            string userinput = Console.ReadLine();
+            decimal deposit = Convert.ToDecimal(userinput);
+            balance = balance + deposit;
+            Console.WriteLine("Deposited" + " " + deposit);
+        }
+    }
+}
---
+++ ObjecterGrundlæggendeProg/ObjecterGrundlæggendeProg/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace ObjecterGrundlæggendeProg
+{
+    class Program : BankAccount
+    {
+
+        static void Main(string[] args)
+        {
+
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                Log("Test1", w);
+            }
+            Console.WriteLine("Choose what you want to do. Withdraw -- 1 Or Deposit -- 2");
+            string userinput = Console.ReadLine();
+            switch(userinput)
+            {
+                case "1":
+                    Withdraw();
+                    break;
+                case "2":
+                    Deposit();
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input");
+                    break;
+            }
+                    Console.ReadKey();
+        }
+        public static void Log(string logMessage, TextWriter w)
+        {
+
+        }
+    }
+}
